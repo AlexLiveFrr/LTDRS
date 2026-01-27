59e7b68bc94e20fcab0c705ef6f405fb9029afec
AlexLiveFrr <AlexLivefrrYt@protonmail.com>
2026-01-27 03:23:28 +0100

    Delete feuille de route directory

--- feuille de route/js/historique.js
+++
@@ -1,9 +0,0 @@
-function sauvegarderDansHistorique(data) {
-    let historique = JSON.parse(localStorage.getItem('historiqueFeuilles') || '[]');
-    historique.push({
-        id: data.id,
-        date: new Date().toLocaleDateString(),
-        trajet: `${data.villeDepart} -> ${data.villeArrivee}`
-    });
-    localStorage.setItem('historiqueFeuilles', JSON.stringify(historique));
-}
